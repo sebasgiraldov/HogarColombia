315bae6bf4a70c7b2cfac799eda3716642aee076
sebastian <sebasgiraldov@gmail.com>
2020-02-04 21:21:11 -0500

    Home page- previo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { FooterComponent } from './public/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
